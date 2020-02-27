986807dfeed6f58b5a4b62fb431302e249774e11
sicel <sicelubuhlen@gmail.com>
2020-02-27 16:40:41 +0200

    NavBar modified but not done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const nav = document.querySelector('#nav');
-
+const opened = document.querySelector('.openNav');
+// const hide = document.querySelector('.everything');
 
 nav.addEventListener('click', ()=>{
 nav.classList.toggle('navActive');
-
+ opened.style.display = 'block'
+ hide.style.display = 'none'
 });
+
+// nav.addEventListener('click',()=> {
+//     opened.style.display = 'none';
+//     hide.style.display = 'block'
+// });
